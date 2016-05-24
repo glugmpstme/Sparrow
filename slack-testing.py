63fb979e5deb3ab862857f0a601526041379c7c1
psiyan <psiyan@users.noreply.github.com>
2016-05-24 22:34:29 +0530

    got basic message store (in-memory), counts, and demo publish/unpublish working

--- slack-testing.py
+++ slack-testing.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 import time
-#for pretty printing the received response
+# for pretty printing the received response
 import pprint
 # never add access tokens directly to code, specially in public code
 import tokenfile
-#see README.md for instructions on importing the SDK
+# see README.md for instructions on importing the SDK
 from slackclient import SlackClient
 
 # specify a local 'tokenfile.py' with the token string from Slack
@@ -19,19 +19,61 @@ pp = pprint.PrettyPrinter(indent=2)
 pp.pprint(sc.api_call('api.test'))
 
 
-class MessageObject:
-	"""docstring for MessageObject"""
-	def __init__(self, message):
-		self.channel = message['channel']
-		self.ts = message['ts']
-		self.content = message
+# class MessageObject:
+# 	"""docstring for MessageObject"""
+# 	def __init__(self, message):
+# 		self.channel = message['channel']
+# 		self.ts = message['ts']
+# 		self.content = message
+
+messagesDict = {} 
+threshold = 1
+
+# TODO: replace with actual publish method
+def publish(message):
+	message['sp-published'] = True
+	pp.pprint(sc.api_call(
+		'chat.postMessage',
+		channel='#social-ops',
+		text='Will publish: ' + str(message),
+		username='pysparrow-testing',
+		icon_emoji=':robot_face:'
+		))
+
+# TODO: replace with actual unpublish method
+def unpublish(message):
+	pp.pprint(sc.api_call(
+		'chat.postMessage',
+		channel='#social-ops',
+		text='Will unpublish: ' + str(message),
+		username='pysparrow-testing',
+		icon_emoji=':robot_face:'
+		))
 
 def storeMessage(message):
-	#if message[]
-	pass
+	tKey = str(message['channel'] + message['ts'])
+	try:
+		messagesDict[tKey]
+	except KeyError:
+		#thumbs up count
+		message['sp-tupCount'] = 0
+		message['sp-published'] = False
+		messagesDict[tKey] = message
+	cyPretty(messagesDict)
 
-def updateCount(reaction):
-	pass
+# delta is either +1 or -1 for add and remove respectively
+def updateCount(reaction, delta):
+	if reaction['reaction'] == '+1':
+		tKey = str(reaction['item']['channel'] + reaction['item']['ts'])
+		try:
+			# TODO: potential underflow
+			messagesDict[tKey]['sp-tupCount'] += delta
+			if messagesDict[tKey]['sp-tupCount'] >= threshold:
+				publish(messagesDict[tKey])
+			elif messagesDict[tKey]['sp-tupCount'] < threshold:
+				unpublish(messagesDict[tKey])
+		except KeyError:
+			print ('[ERR] Message not found in db!')
 
 def cyPretty(response):	
 	pp.pprint(response)
@@ -43,11 +85,13 @@ if sc.rtm_connect():
 		for response in responseArray:
 			if response['type'] == 'message':
 				storeMessage(response)
-			if response['type'] == 'reaction_added' or \
-			response['type'] == 'reaction_removed':
-				cyPretty(response)
+			elif response['type'] == 'reaction_added':
+				updateCount(response, 1)
+			elif response['type'] == 'reaction_removed':
+				updateCount(response, -1)
 			else:
-				print (response['type']);
+				cyPretty(response)
+				#print (response['type']);
 		time.sleep(1)
 else:
 	pp.pprint(sc.rtm_connect())
