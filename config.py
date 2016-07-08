94059f9a671b4866a48699bb7a076f81f60273d1
bumblebee16 <aishwarya1604@gmail.com>
2016-07-08 15:12:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #config
 
-consumer_key = ""
-consumer_secret = ""
-access_key = ""
-access_secret = ""
+consumer_key = "vCLoSIePS2W99k1GwrdB8Zb0N"
+consumer_secret = "O8cfnimD8tFhswDBY7FZ8b9nxcQUZQuVrTL1xhCJu3NeSySMIo"
+access_key = "3150003822-hzO2YAK0sVlbcwYX3nWpF2TU4XfPFkKr9Neq17B"
+access_secret = "JquojU5eygnFzv6rj36Vb3Speyzlb5WBvZrJUlLR7eHpg"
