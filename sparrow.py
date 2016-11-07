f84866dace44577551b5dee58ba25185db463314
darkuswraith <jmjoymallik@gmail.com>
2016-11-07 23:25:19 +0530

    script for posting links

---
+++ sparrow.py
@@ -0,0 +1,27 @@
+import facebook
+
+def main():
+  cfg = {
+    "page_id"      : "213596285740034",  # Step 1
+    "access_token" : ""   # Step 3
+    }
+
+  api = get_api(cfg)
+  msg = "Hello, world!"
+  status = api.put_wall_post(msg)
+
+def get_api(cfg):
+  graph = facebook.GraphAPI(cfg['access_token'])
+  # Get page token to post as the page. You can skip
+  # the following if you want to post as yourself.
+  resp = graph.get_object('me/accounts')
+  page_access_token = None
+  for page in resp['data']:
+    if page['id'] == cfg['page_id']:
+      page_access_token = page['access_token']
+  graph = facebook.GraphAPI(page_access_token)
+  return graph
+  # You can also skip the above if you get a page token
+
+if __name__ == "__main__":
+  main()
